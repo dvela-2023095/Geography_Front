2f89f385d2783c6531284a036fc5dad136a943a3
sveliz2023124 <sveliz-2023124@kinal.edu.gt>
2025-09-02 10:46:04 -0600

    Se agregan funcionalidades de progress

---
+++ services/ProgressApi.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+
+const apiClient = axios.create({
+  baseURL: "http://localhost:2636/v1/progress/",
+  timeout: 4000,
+})
+
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token")
+  if (token) config.headers.authorization = token
+  return config
+})
+
+export const getProgressByUser = async (userId) => {
+  try {
+    const res = await apiClient.get(`byUser/${userId}`)
+    return res.data
+  } catch (e) {
+    return { error: true, message: "No se pudo obtener el progreso", e }
+  }
+}
